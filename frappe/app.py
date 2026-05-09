751daec655a7a9bb8d86d53d03afa67a15dc9e07
DAT-cy <dat224k@gmail.com>
2026-05-09 02:44:54 +0000

    fix : add config

--- frappe/app.py
+++ frappe/app.py
@@ -184,7 +184,16 @@ def init_request(request):
 			raise frappe.SessionStopped("Session Stopped")
 	else:
 		frappe.connect(set_admin_as_user=False)
-	if request.path.startswith("/api/method/upload_file"):
+	# [UPDATED] Bỏ giới hạn dung lượng cho drive upload API
+	# [OLD CODE]
+	# if request.path.startswith("/api/method/upload_file"):
+	# 	from frappe.core.api.file import get_max_file_size
+	# 	request.max_content_length = get_max_file_size()
+	# else:
+	# 	request.max_content_length = cint(frappe.local.conf.get("max_file_size")) or 25 * 1024 * 1024
+	if request.path.startswith("/api/method/drive.api.files"):
+		request.max_content_length = None  # Không giới hạn dung lượng cho drive API
+	elif request.path.startswith("/api/method/upload_file"):
 		from frappe.core.api.file import get_max_file_size
 
 		request.max_content_length = get_max_file_size()
